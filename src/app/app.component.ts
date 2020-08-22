5991dd0f83e5679b81622d199907c03d673781ad
samirod <a.roodaki@sinacomsys.com>
2020-08-22 13:31:49 +0430

    toolbar component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sina-server-manager';
+  title = 'sina server manager';
 }
